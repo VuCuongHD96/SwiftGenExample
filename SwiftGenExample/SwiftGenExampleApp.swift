2c5e9c0f7b8c8c33c792c3c0be225747752f6850
VuCuongHD96 <xuancuonghd1102@gmail.com>
2023-12-21 21:51:25 +0700

    Initial Commit

---
+++ SwiftGenExample/SwiftGenExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftGenExampleApp.swift
+//  SwiftGenExample
+//
+//  Created by Work on 21/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftGenExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
